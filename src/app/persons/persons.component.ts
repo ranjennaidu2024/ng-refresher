6cf4662a24c8f0df599fd97557d26ff5201b12e4
ranjen_vasudiven <ranjen.vasudiven@smarttradzt.com>
2024-08-09 17:15:33 +0800

    Pushing Data Around with Subjects

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,20 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PersonsService } from './persons.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html',
 })
-export class PersonsComponent implements OnInit {
+export class PersonsComponent implements OnInit, OnDestroy {
   personList: string[];
+  private personListSubs: Subscription;
 
   constructor(private prsService: PersonsService) {}
 
   ngOnInit() {
     this.personList = this.prsService.persons;
+    this.personListSubs = this.prsService.personsChanged.subscribe(
+      (persons) => {
+        this.personList = persons;
+      }
+    );
   }
 
   onRemovePerson(personName: string) {
     this.prsService.removePerson(personName);
+  }
+
+  ngOnDestroy() {
+    this.personListSubs.unsubscribe();
   }
 }
